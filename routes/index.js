924221e1ef76e963401a52bd2f276dafae195376
nitro-neal <groessler24@gmail.com>
2019-09-06 14:09:48 -0500

    Have profit hashmap for 30 days

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const axios = require("axios");
 const fs = require("fs");
 const moment = require("moment");
-// const historicBnbPriceData = require("./bnb-historic-price.json");
-// import sdfas from ("")
-// var someObject = require('./somefile.json')
-// import axios from "axios";
+
+let coinPriceMaps = new Map();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -15,67 +13,149 @@ router.get("/", function(req, res, next) {
 
 start();
 
+function getTopPerformers(numberOfDays) {
+  // coinPriceMaps.forEach((key, value) => {
+  //   console.log(key);
+  // });
+
+  // for (const [key, value] of Object.entries(coinPriceMaps)) {
+  //   console.log(key, value);
+  // }
+
+  // for (let key in coinPriceMaps) {
+  //   console.log(key);
+  // }
+
+  // for (const entry of coinPriceMaps.entries()) {
+  //   console.log(entry);
+  // }
+
+  for (const [key, value] of coinPriceMaps.entries()) {
+    // console.log(key, value);
+    if (value.length < numberOfDays - 1) {
+      // not enough days recorded yet
+      continue;
+    }
+
+    // For example, if you buy a stock today for $50, and tomorrow the stock is worth $52, your percentage gain is 4% ([$52 - $50] / $50).
+
+    console.log(" start " + key);
+    console.log(value);
+    console.log(value[value.length - 1]);
+    console.log(value[value.length - numberOfDays]);
+    let percentChangeFromToday =
+      (value[value.length - 1] - value[value.length - numberOfDays]) /
+      value[value.length - numberOfDays];
+
+    console.log(key + " ---> " + (percentChangeFromToday * 100).toFixed(2));
+  }
+}
+
+async function generateCoinPriceMaps() {
+  let coinsJson = await axios.get("https://dex.binance.org/api/v1/ticker/24hr");
+
+  for (let i = 0; i < coinsJson.data.length; i++) {
+    let prices = [];
+    let coin = coinsJson.data[i];
+
+    if (coin.baseAssetName === "BNB" || coin.baseAssetName === "BTCB-1DE") {
+      continue;
+    }
+
+    let workingDate = new moment();
+    for (let i = 30; i > 0; i--) {
+      workingDate.subtract(1, "days");
+      // console.log(yesterday);
+      // console.log(workingDate);
+      let path = getFilePath(workingDate, coin.baseAssetName);
+
+      try {
+        // console.log("reading path:   " + path);
+        let dayData = JSON.parse(fs.readFileSync(path, "utf-8"));
+        prices.push(dayData.usdPrice);
+      } catch (e) {
+        console.log("price data does not go back that far - " + path);
+      }
+    }
+
+    coinPriceMaps.set(coin.baseAssetName, prices);
+  }
+
+  // console.log("FTM DATA");
+  // console.log(coinPriceMaps.get("FTM-A64"));
+  getTopPerformers(30);
+}
+
+// https://dex.binance.org/api/v1/klines?symbol=FTM-A64_BNB&interval=5m
 async function start() {
-  let bnbHistoricPriceMap = getBnbHistoricPriceMap();
-  // https://dex.binance.org/api/v1/klines?symbol=FTM-A64_BNB&interval=5m
+  let bnbHistoricPriceMap = await getBnbHistoricPriceMap();
+
   console.log("started");
-  // writeFile();
 
   let coinsJson = await axios.get("https://dex.binance.org/api/v1/ticker/24hr");
 
-  // for (let i = 0; i < coinsJson.data.length; i++) {
-  //   let coin = coinsJson.data[i];
-  //   console.log(new moment().day());
-  //   console.log(moment().format("YYYY-MM-DD"));
-  //   console.log(coin);
-  //   console.log(coin.baseAssetName);
-  //   // msleep(3000);
+  for (let i = 0; i < coinsJson.data.length; i++) {
+    let coin = coinsJson.data[i];
+    let today = new moment();
 
-  //   let today = new moment();
+    if (
+      !doesDateExist(today.subtract(1, "days"), coin.baseAssetName) &&
+      coin.baseAssetName !== "BNB" &&
+      coin.baseAssetName !== "BTCB-1DE"
+    ) {
+      let result = await axios.get(
+        "https://dex.binance.org/api/v1/klines?symbol=" +
+          coin.baseAssetName +
+          "_BNB&interval=1d"
+      );
 
-  //   if (doesDateExist(today, coin.baseAssetName)) {
-  //     // file is saved already
-  //   } else {
-  //     let result = await axios.get(
-  //       "https://dex.binance.org/api/v1/klines?symbol=" +
-  //         coin.baseAssetName +
-  //         "_BNB&interval=1d"
-  //     );
+      console.log("Saving coin info for: " + coin.baseAssetName);
+      saveCoinInfo(coin.baseAssetName, result.data, bnbHistoricPriceMap);
+      msleep(3000);
+    }
+  }
 
-  //     saveCoinInfo(coin.baseAssetName, result.data);
-  //     msleep(3000);
-  //   }
-  // }
+  console.log("\n\n FINISHED WITH SAVING TO DISK! \n\n");
+
+  await generateCoinPriceMaps();
+
+  console.log("\n\n FINISHED WITH CREATING MAPS! \n\n");
 }
 
 async function getBnbHistoricPriceMap() {
   let bnbHistoricPriceMap = new Map();
+  let coinMarketCapHistoricData = JSON.parse(
+    fs.readFileSync("bnbHistoricPrice.json", "utf-8")
+  );
 
   let bnbTusd = await axios.get(
     "https://dex.binance.org/api/v1/klines?symbol=BNB_TUSDB-888&interval=1d"
   );
 
+  // This only has recent price data
   for (let i = 0; i < bnbTusd.data.length; i++) {
     let bnbDay = moment(parseInt(bnbTusd.data[i][0]));
     let year = bnbDay.format("YYYY");
     let month = bnbDay.format("MM");
     let day = bnbDay.format("DD");
-    bnbHistoricPriceMap.set(year + "-" + month + "-" + day, bnbTusd.data[i][1]);
-    console.log(
-      year + "-" + month + "-" + day + " -----> " + bnbTusd.data[i][1]
+    bnbHistoricPriceMap.set(
+      year + "-" + month + "-" + day,
+      parseFloat(bnbTusd.data[i][1])
     );
   }
 
-  // console.log(historicBnbPriceData);
-  let coinMarketCapHistoricData = JSON.parse(
-    fs.readFileSync("bnbHistoricPrice.json", "utf-8")
-  );
-
-  console.log(coinMarketCapHistoricData);
-
-  // for(coinMarketCapHistoricData) {
-  //   bnbHistoricPriceMap.set(year + "-" + month + "-" + day, bnbTusd.data[i][1]);
-  // }
+  for (let i = 0; i < coinMarketCapHistoricData.length; i++) {
+    let dayMoment = moment(coinMarketCapHistoricData[i].date);
+    let year = dayMoment.format("YYYY");
+    let month = dayMoment.format("MM");
+    let day = dayMoment.format("DD");
+    if (!bnbHistoricPriceMap.has(year + "-" + month + "-" + day)) {
+      bnbHistoricPriceMap.set(
+        year + "-" + month + "-" + day,
+        coinMarketCapHistoricData[i].open
+      );
+    }
+  }
   return bnbHistoricPriceMap;
 }
 
@@ -112,39 +192,34 @@ function doesDateExist(date, baseAssetName) {
   }
 }
 
-function saveCoinInfo(baseAssetName, historicPrice) {
-  // https://docs.binance.org/api-reference/dex-api/paths.html#apiv1klines
-  // [ 1567468800000,
-  //   '0.00621742',
-  //   '0.00627680',
-  //   '0.00608650',
-  //   '0.00620718',
-  //   '482934.00000000',
-  //   1567555199999,
-  //   '2997.38388808',
-  //   1166 ]
+// https://docs.binance.org/api-reference/dex-api/paths.html#apiv1klines
 
-  // [
-  //   1499040000000,      // Open time
-  //   "0.01634790",       // Open
-  //   "0.80000000",       // High
-  //   "0.01575800",       // Low
-  //   "0.01577100",       // Close
-  //   "148976.11427815",  // Volume
-  //   1499644799999,      // Close time
-  //   "2434.19055334",    // Quote asset volume
-  //   308                // Number of trades
-  // ]
+// [
+//   1499040000000,      // Open time
+//   "0.01634790",       // Open
+//   "0.80000000",       // High
+//   "0.01575800",       // Low
+//   "0.01577100",       // Close
+//   "148976.11427815",  // Volume
+//   1499644799999,      // Close time
+//   "2434.19055334",    // Quote asset volume
+//   308                // Number of trades
+// ]
 
+function saveCoinInfo(baseAssetName, historicPrice, bnbHistoricPriceMap) {
   for (let i = 0; i < historicPrice.length; i++) {
     let historicPriceDay = historicPrice[i];
 
     let day = moment(parseInt(historicPriceDay[0]));
 
-    console.log(day.format("YYYY-MM-DD"));
-
     if (!doesDateExist(day, baseAssetName)) {
       let filePath = getFilePath(day, baseAssetName);
+
+      let year = day.format("YYYY");
+      let month = day.format("MM");
+      let d = day.format("DD");
+
+      let usdPrice = bnbHistoricPriceMap.get(year + "-" + month + "-" + d);
 
       let dayObject = {
         openTime: historicPriceDay[0],
@@ -155,8 +230,17 @@ function saveCoinInfo(baseAssetName, historicPrice) {
         volume: historicPriceDay[5],
         closeTime: historicPriceDay[6],
         assetVolume: historicPriceDay[7],
-        numberOfTrades: historicPriceDay[8]
+        numberOfTrades: historicPriceDay[8],
+        usdOpenPrice: usdPrice * parseFloat(historicPriceDay[1]),
+        usdPrice:
+          (usdPrice *
+            (parseFloat(historicPriceDay[1]) +
+              parseFloat(historicPriceDay[4]))) /
+          2,
+        bnbPrice: bnbHistoricPriceMap.get(year + "-" + month + "-" + d)
       };
+
+      // console.log(dayObject);
 
       let stream = fs.createWriteStream(filePath);
       stream.once("open", function(fd) {
